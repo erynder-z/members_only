de0bc03fdaa95282dd6afcb3dc261486f6aebf72
erynder-z <stfn.bgr@gmail.com>
2022-11-25 11:14:37 +0100

    Update membership status via password

--- controllers/bouncerController.js
+++ controllers/bouncerController.js
@@ -1,3 +1,42 @@
+const { body, validationResult } = require('express-validator');
+const env = require('dotenv').config().parsed;
+const User = require('../models/user');
+
 exports.bouncer = (req, res) => {
   res.render('bouncer', { user: req.user });
 };
+
+exports.become_member = [
+  body('secretPassword')
+    .custom((value, { req }) => {
+      if (value !== `${env.MEMBER_PASSWORD}`) {
+        throw new Error('Wrong password');
+      }
+      // Indicates the success of this synchronous custom validator
+      return true;
+    })
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('bouncer', {
+        user: req.user,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    User.findByIdAndUpdate(
+      req.user.id,
+      { membership_status: 'MEMBER' },
+      (err, result) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(result.url);
+      }
+    );
+  },
+];
--- routes/clubhouse.js
+++ routes/clubhouse.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const login_controller = require('../controllers/loginController');
+const bouncer_controller = require('../controllers/bouncerController');
 
 // index
 router.get('/', login_controller.index);
+
+// bouncer awaiting secret password
+router.get('/bouncer', bouncer_controller.bouncer);
+
+router.post('/bouncer/become-member', bouncer_controller.become_member);
 
 module.exports = router;
