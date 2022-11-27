2fd75174b2068199dceb9c32859215852d4aca58
erynder-z <stfn.bgr@gmail.com>
2022-11-27 17:03:27 +0100

    Update routes

--- routes/clubhouse.js
+++ routes/clubhouse.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const login_controller = require('../controllers/loginController');
 const bouncer_controller = require('../controllers/bouncerController');
+const user_controller = require('../controllers/userController');
+const message_controller = require('../controllers/messageController');
 
 // index
 router.get('/', login_controller.index);
@@ -11,5 +13,13 @@ router.get('/', login_controller.index);
 router.get('/bouncer', bouncer_controller.bouncer);
 
 router.post('/bouncer/become-member', bouncer_controller.become_member);
+
+router.get('/user/:id', user_controller.user_page);
+
+router.get('/create_message', message_controller.create_message_get);
+
+router.post('/create_message', message_controller.create_message_post);
+
+router.get('/message/:id', message_controller.message_detail_get);
 
 module.exports = router;
