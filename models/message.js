ee5f05721a68fb389e700b97acdb90914d711a64
erynder-z <stfn.bgr@gmail.com>
2022-11-27 17:02:53 +0100

    Add formatted timestamp virtual

--- models/message.js
+++ models/message.js
@@ -1,3 +1,4 @@
+const { DateTime } = require('luxon');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -5,13 +6,20 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
   msg_title: { type: String, required: true },
   msg_text: { type: String, required: true },
-  msg_timestamp: { type: Date, required: true },
+  msg_timestamp: { type: Date, required: true, default: Date.now },
   msg_author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 // Virtual for message's URL
 MessageSchema.virtual('url').get(function () {
   return `/clubhouse/message/${this._id}`;
+});
+
+// Virtual for formatting the date
+MessageSchema.virtual('timestamp_formatted').get(function () {
+  return DateTime.fromJSDate(this.msg_timestamp).toLocaleString(
+    DateTime.DATE_MED
+  );
 });
 
 // Export model
